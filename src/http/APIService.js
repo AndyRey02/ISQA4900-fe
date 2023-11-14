ca69682dbd0132398a2bf234ab388938b1204112
UNOmislas <mislas@unomaha.edu>
2023-11-13 19:33:01 -0600

    Backend URL

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Change the API_URL to the correct location of the backend API before deploying the app
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = 'https://99mislas.pythonanywhere.com'
 
   export class APIService {
     constructor() {
